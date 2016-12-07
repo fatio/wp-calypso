fbe4a4e7c6bb1af5a4a8aa2dff6a57f0e6849aea
Marin Atanasov <tyxla@abv.bg>
2016-12-07 15:25:21 +0200

    Jetpack Settings: Add reducer for updating and requesting Jumpstart

--- client/state/jetpack-settings/reducer.js
+++ client/state/jetpack-settings/reducer.js
@@ -7,7 +7,9 @@ import { combineReducers } from 'redux';
  * Internal dependencies
  */
 import { reducer as jetpackModules } from './modules/reducer';
+import { reducer as jetpackJumpstart } from './jumpstart/reducer';
 
 export default combineReducers( {
-	jetpackModules
+	jetpackModules,
+	jetpackJumpstart
 } );
